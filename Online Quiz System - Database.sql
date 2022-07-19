369bd1ade415720ca783abcf5713e2729874018f
UdayUSR <udayfkbd@gmail.com>
2022-07-19 19:13:45 +0600

    Final

--- Online Quiz System - Database.sql
+++ Online Quiz System - Database.sql
@@ -72,6 +72,11 @@ INSERT INTO teacher VALUES(1002, 'Professor Dr. Dahlia Bailey', 28, 70000,  'EEE
 INSERT INTO teacher VALUES(1003, 'Professor Dr. Axel Young', 32, 80000, 'CSE', 'rJ5iyHfw');
 INSERT INTO teacher VALUES(1004, 'Professor Dr. Jessica Morgan', 29, 72000, 'ME', 'Z1WrN39W');
 INSERT INTO teacher VALUES(1005, 'Professor Dr. Tobias Douglas', 40, 90000, 'CE', 'R73Hg7kk');
+INSERT INTO teacher VALUES(1006, 'Professor Dr. Cecilia Cooper', 42, 94000, 'CSE', 'iEB34V15');
+INSERT INTO teacher VALUES(1007, 'Professor Dr. Jude Lee', 31, 80000, 'CSE', 'N693FyGA');
+INSERT INTO teacher VALUES(1008, 'Professor Dr. Ryker Clark', 34, 85000, 'CSE', 'q456M1U8');
+INSERT INTO teacher VALUES(1009, 'Professor Dr. Troy Johnson', 43, 106000, 'CSE', 'kF2841bJ');
+INSERT INTO teacher VALUES(1010, 'Professor Dr. Alexander Reyes', 28, 75000, 'CE', 'swPS231Z');
 
 
 INSERT INTO student VALUES(1807001, 'Cody Fleming', 'CSE', 'AHQWr8Rc');
@@ -82,6 +87,13 @@ INSERT INTO student VALUES(1805001, 'Hallie Hughes', 'CE', 'LudbW93R');
 INSERT INTO student VALUES(1807003, 'Pablo Diaz', 'CSE', 'Fk24te87');
 INSERT INTO student VALUES(1807004, 'Ryleigh Hopkins', 'CSE', 'b8I3VJWo');
 INSERT INTO student VALUES(1807005, 'Leonardo Jones', 'CSE', 'lU023VO8');
+INSERT INTO student VALUES(1807006, 'Jerry Turner', 'CSE', '5g42Gqqd');
+INSERT INTO student VALUES(1807007, 'Kyler Harris', 'CSE', '3Td1XUb3');
+INSERT INTO student VALUES(1807008, 'Kaiden Morales', 'CSE', '20R4aX9D');
+INSERT INTO student VALUES(1807009, 'Cristian Foster', 'CSE', 'Eajh8C90');
+INSERT INTO student VALUES(1807010, 'Thomas Thomas', 'CSE', '2uIa9H7F');
+INSERT INTO student VALUES(1803002, 'Ryder Gilbert', 'EEE', '1HZo6U7T');
+INSERT INTO student VALUES(1805002, 'Malia Larson', 'CE', 'wpD42n9f');
 
 
 INSERT INTO quiz VALUES(1, 'CSE', 'Database', '12-JUNE-2022', '1003');
@@ -89,6 +101,12 @@ INSERT INTO quiz VALUES(2, 'EEE', 'Digital Electronics', '15-JUNE-2022', '1002')
 INSERT INTO quiz VALUES(3, 'CE', 'Surveying', '20-JUNE-2022', '1005');
 INSERT INTO quiz VALUES(4, 'ME', 'Mechanical Drawing', '28-JUNE-2022', '1004');
 INSERT INTO quiz VALUES(5, 'CSE', 'OOP', '01-JULY-2022', '1001');
+INSERT INTO quiz VALUES(6, 'CSE', 'TOC', '03-JULY-2022', '1006');
+INSERT INTO quiz VALUES(7, 'CSE', 'SOFTWARE', '05-JULY-2022', '1007');
+INSERT INTO quiz VALUES(8, 'CSE', 'CPP', '10-JULY-2022', '1008');
+INSERT INTO quiz VALUES(9, 'CSE', 'PERIPHERAL', '15-JULY-2022', '1009');
+INSERT INTO quiz VALUES(10, 'CSE', 'C', '18-JULY-2022', '1001');
+
 
 
 INSERT INTO question VALUES(1, 1, 'RDBMS is an acronym for', '(A) Relational Database', '(B) Relational Database Merging System', '(C) Relational Database Management System', '(D) Relational Database Manipulation System','C');
@@ -96,7 +114,12 @@ INSERT INTO question VALUES(1, 2, 'The process of performing corrections on the 
 INSERT INTO question VALUES(1, 3, 'Database management systems ___ data', '(A) Delete', '(B) Share', '(C) Process', '(D) Store', 'B');
 INSERT INTO question VALUES(1, 4, 'Hierarchical database was primarily used on', '(A) Business Computers', '(B) Mainframe Computers', '(C) Personal Computers', '(D) Super Computers', 'B');
 INSERT INTO question VALUES(1, 5, 'DBMS is an acronym for', '(A) Database Merging System', '(B) Database', '(C) Database Management System', '(D) Database Manipulating System', 'C');
---INSERT INTO question VALUES(1, 6, '', '', '', '', '', '');
+INSERT INTO question VALUES(5, 1, 'When OOP concept did first came?', 'a) 1980s', 'b)1995', 'c) 1970s', 'd) 1993', 'C');
+INSERT INTO question VALUES(5, 2, 'How many types of access specifiers are there?', 'a) 4', 'b) 3', 'c) 2', 'd) 1', 'B');
+INSERT INTO question VALUES(5, 3, 'Which access specifier is usually used for data members of a class?', 'a) Protected', 'b) Private', 'c) Public', 'd) Default', 'B');
+INSERT INTO question VALUES(5, 4, 'Which feature of OOP reduces the use of nested classes?', 'a) Inheritance', 'b) Binding', 'c) Abstraction', 'd) Encapsulation', 'A');
+INSERT INTO question VALUES(5, 5, 'Where is the memory allocated for the objects?', 'a) Cache', 'b) ROM', 'c) HDD', 'd) RAM', 'D');
+
 
 
 INSERT INTO result VALUES(1, 1807001, 5);
@@ -104,12 +127,21 @@ INSERT INTO result VALUES(1, 1807002, 4);
 INSERT INTO result VALUES(1, 1807003, 3);
 INSERT INTO result VALUES(1, 1807004, 0);
 INSERT INTO result VALUES(1, 1807005, 5);
-
+INSERT INTO result VALUES(1, 1807006, 5);
+INSERT INTO result VALUES(1, 1807007, 3);
+INSERT INTO result VALUES(1, 1807008, 4);
+INSERT INTO result VALUES(1, 1807009, 2);
+INSERT INTO result VALUES(1, 1807010, 0);
 INSERT INTO result VALUES(5, 1807001, 2);
 INSERT INTO result VALUES(5, 1807002, 5);
 INSERT INTO result VALUES(5, 1807003, 4);
 INSERT INTO result VALUES(5, 1807004, 2);
 INSERT INTO result VALUES(5, 1807005, 0);
+INSERT INTO result VALUES(5, 1807006, 5);
+INSERT INTO result VALUES(5, 1807007, 4);
+INSERT INTO result VALUES(5, 1807008, 5);
+INSERT INTO result VALUES(5, 1807009, 1);
+INSERT INTO result VALUES(5, 1807010, 3);
 
 
 SELECT * FROM teacher;
@@ -119,21 +151,21 @@ SELECT * FROM question;
 SELECT * FROM result;
 
 
---ADDING NEW COLUMN 
+--ADDING NEW COLUMN STUDENT_AGE TO STUDENT
 ALTER TABLE student 
  ADD student_age NUMBER(2);
 
 DESCRIBE student;
 
 
---MODIFYING COLUMN
+--MODIFYING COLUMN STUDENT_AGE
 ALTER TABLE student
  MODIFY student_age NUMBER(3);
 
 DESCRIBE student;
 
 
---RENAMING COLUMN
+--RENAMING COLUMN STUDENT_AGE TO S_AGE
 ALTER TABLE student
  RENAME COLUMN student_age to s_age;
 
@@ -142,22 +174,22 @@ DESCRIBE student;
 SELECT * from student;
 
 
---ADDING VALUE
-INSERT INTO student VALUES(1807006, 'Juniper Harrison', 'CSE', '6YX9tRbQ', 22);
+--ADDING VALUE TO STUDENT
+INSERT INTO student VALUES(1807011, 'Juniper Harrison', 'CSE', '6YX9tRbQ', 22);
 SELECT * from student;
 
 
---UPDATING VALUE
-UPDATE student SET s_age=26 where student_id=1807006;
+--UPDATING VALUE IN STUDENT
+UPDATE student SET s_age=26 where student_id=1807011;
 SELECT * from student;
 
 
---DELETING VALUE
+--DELETING VALUE FROM STUDENT
 DELETE FROM student WHERE s_age=26;
 SELECT * from student;
 
 
---DROPING COLUMN
+--DROPING COLUMN S_AGE
 ALTER TABLE student
  DROP COLUMN s_age;
 
@@ -172,16 +204,16 @@ SELECT student_id, student_name FROM student;
 SELECT * from student;
 
 
---Use of DISTINCT
+--Use of DISTINCT TO SHOW DEPARTMENTS
 SELECT student_dept FROM student;
 SELECT DISTINCT student_dept FROM student;
 
---Calculated Fields
+--Calculated Fields TO CONVERT MARKS TO 10
 SELECT student_id, marks FROM result WHERE quiz_id=1;
 SELECT student_id, (marks*2) FROM result WHERE quiz_id=1;
 
 --Giving the column a different name
-SELECT (marks*2) FROM result
+SELECT (marks*2) FROM result;
 SELECT (marks*2) AS increased_marks FROM result;
 
 --Comparison Search Condition
@@ -262,13 +294,12 @@ SELECT COUNT(*) FROM teacher;
 SELECT COUNT(DISTINCT(teacher_salary)) FROM teacher;
 SELECT SUM(teacher_salary) FROM teacher;
 SELECT AVG(teacher_salary) FROM teacher;
-SELECT AVG( NVL(teacher_salary) ) FROM teacher;
 SELECT MIN(teacher_salary) FROM teacher;
 SELECT MAX(teacher_salary) FROM teacher;
 
 
 
---GROUP BY clause
+--GROUP BY clause TO SHOW TOTAL MARKS
 SELECT student_id, SUM(marks) FROM result
 GROUP BY student_id;
 
@@ -357,11 +388,6 @@ WHERE teacher_age=32;
 SELECT q.quiz_id, q.category , r.student_id, r.marks
 FROM quiz q Join result r
 ON q.quiz_id=r.quiz_id;
-
---Join(USING)
-SELECT q.quiz_id, q.category , r.student_id, r.marks
-FROM quiz q Join result r
-USING (quiz_id);
 
 
 --Natural Join
@@ -507,7 +533,7 @@ BEGIN
 END;
 /
 
---RETURN AVERAGE SALARY OF TEACHER
+--RETURN AVERAGE SALARY OF TEACHER USING FUNCTION
 CREATE OR REPLACE FUNCTION avg_salary RETURN NUMBER IS
     avg_salary teacher.teacher_salary%type;
 BEGIN
@@ -536,7 +562,7 @@ END IF;
 END;
 /
 
-INSERT INTO teacher VALUES(1009, 'Professor Dr. Flemming', 31, 76000, 'CSE', 'AvK5WQ77');
+INSERT INTO teacher VALUES(1011, 'Professor Dr. Flemming', 31, 76000, 'CSE', 'AvK5WQ77');
 
 
 --TRIGGER ON STUDENT MARKS
@@ -605,6 +631,7 @@ SELECT category, sysdate-quiz_date AS DAYS_PASSED
 FROM quiz;
 
 --View for students of CSE department
+DROP VIEW CSE_STUDENT;
 CREATE VIEW CSE_STUDENT AS
 SELECT * FROM student
 WHERE student_dept='CSE';
